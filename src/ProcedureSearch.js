8534f7bacec0321f30c5c4f047e28d460a57f008
Gual Escalante <gual23@gmail.com>
2018-07-27 10:51:05 -0600

    Cleaned up search component

--- src/ProcedureSearch.js
+++ src/ProcedureSearch.js
@@ -3,16 +3,17 @@ import React, { Component } from 'react'
 export default class ProcedureSearch extends Component {
   render() {
     return (
-      <div>
-        <form>
-            <div className="form-group">
-                <label for="searchTerm">Término de búsqueda</label>
-                <input type="text" className="form-control" aria-describedby="searchHelp" placeholder="Ingresar término" />
-                    <small id="searchHelp" className="form-text text-muted">Se utilizará para filtrar los trámites.</small>
-            </div>
-            <button type="submit" className="btn btn-primary">Buscar</button>
-        </form>
-      </div>
+      <form className="searchForm">
+        <div className="form-group">
+          <label for="searchTerm">Término de búsqueda</label>
+          <input type="text" className="form-control" aria-describedby="searchHelp"
+            placeholder="Ingresar término" />
+              <small id="searchHelp" className="form-text text-muted">
+                Se utilizará para filtrar los trámites.
+              </small>
+        </div>
+        <button type="submit" className="btn btn-primary">Buscar</button>
+      </form>
     )
   }
 }
