a8c2a6c5d3f050387ce31129acc2c31075b6bded
Gual Escalante <gual23@gmail.com>
2018-09-01 18:45:55 -0600

    Updated components files

---
+++ src/screens/UI/FAQ.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class FAQ extends Component {
+  render() {
+    return (
+      <section className="pb_cover_v3 overflow-hidden cover-bg-indigo cover-bg-opacity text-left pb_gradient_v1 pb_slant-light" id="section-home">
+        <div className="container">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-md-6">
+              <h2 className="heading mb-3">gob.sv </h2>
+              <div className="sub-heading">
+                <p className="mb-4">Bienvenido a tu portal para consulta de Trámites. Podrás encontrar información sobre los trámites de los 14 ministerios.</p>
+              </div>
+            </div>
+            <div className="col-md-1">
+            </div>
+            <h3>FAQ</h3>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
---
+++ src/screens/UI/NavBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark pb_navbar pb_scrolled-light" id="pb-navbar">
+        <div className="container">
+          <Link className="navbar-brand" to="/">Gob.sv</Link>
+          <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#probootstrap-navbar" aria-controls="probootstrap-navbar"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span><i className="ion-navicon"></i></span>
+          </button>
+          <div className="collapse navbar-collapse" id="probootstrap-navbar">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item"><Link className="nav-link" to="/data">Datos</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/categories">Categorías</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/about">Acerca de</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/contact">Contacto</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/faq">Preguntas frecuentes</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
