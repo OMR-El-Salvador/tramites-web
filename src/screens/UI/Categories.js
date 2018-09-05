bf0dde82db10afbcd02b8b590ed98ee0ee160e52
Gual Escalante <gual23@gmail.com>
2018-09-05 09:49:17 -0600

    Added grouping selector

--- src/screens/UI/Categories.js
+++ src/screens/UI/Categories.js
@@ -136,9 +136,22 @@ export default class Categories extends Component {
       <div>
         <section>
           <div className='container'>
-            <div className='row justify-content-center mb-5'>
+            {/* <div className='row justify-content-center mb-5'>
               <div className='col-md-6 text-center mb-5'>
-                {/* <h5 className='text-uppercase pb_font-15 mb-2 pb_color-dark-opacity-3 pb_letter-spacing-2'><strong>Features</strong></h5> */}
+                <h5 className='text-uppercase pb_font-15 mb-2 pb_color-dark-opacity-3 pb_letter-spacing-2'><strong>Features</strong></h5>
+              </div>
+            </div> */}
+            <div className='row group-by'>
+              <div className='col-md-3 offset-md-9'>
+                <form>
+                  <div className='form-group row'>
+                    <select className='form-control-lg form-control' id='group-by'>
+                      <option>Categorías</option>
+                      <option>Ministerios</option>
+                      <option>Orientación</option>
+                    </select>
+                  </div>
+                </form>
               </div>
             </div>
             <div className='row'>
