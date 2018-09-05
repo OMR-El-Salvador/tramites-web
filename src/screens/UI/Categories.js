676528431ceb9f97ae51ce3e6f6abd58659e52df
Gual Escalante <gual23@gmail.com>
2018-09-04 23:08:48 -0600

    Moved categories to component

--- src/screens/UI/Categories.js
+++ src/screens/UI/Categories.js
@@ -3,18 +3,75 @@ import React, { Component } from 'react'
 export default class Categories extends Component {
   render() {
     return (
-      <section className="pb_cover_v3 overflow-hidden cover-bg-indigo cover-bg-opacity text-left pb_gradient_v1 pb_slant-light" id="section-home">
-        <div className="container">
-          <div className="row align-items-center justify-content-center">
-            <div className="col-md-6">
-              <h2 className="heading mb-3">gob.sv </h2>
-              <div className="sub-heading">
-                <p className="mb-4">Bienvenido a tu portal para consulta de Trámites. Podrás encontrar información sobre los trámites de los 14 ministerios.</p>
+      <section class="pb_section bg-light pb_pb-250" id="section-features">
+        <div class="container">
+          <div class="row justify-content-center mb-5">
+            <div class="col-md-6 text-center mb-5">
+              {/* <h5 class="text-uppercase pb_font-15 mb-2 pb_color-dark-opacity-3 pb_letter-spacing-2"><strong>Features</strong></h5> */}
+              <h2>Categorías</h2>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-4 col-md- col-sm-6">
+              <div class="media d-block pb_feature-v1 text-left">
+                <div class="pb_icon"><i class="fas fa-users fa-xs pb_icon-gradient"></i></div>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-3 heading">Beneficios y Programas Sociales</h5>
+                  <p class="text-sans-serif">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                    there live the blind texts.</p>
+                </div>
               </div>
             </div>
-            <div className="col-md-1">
+            <div class="col-lg-4 col-md- col-sm-6">
+              <div class="media d-block pb_feature-v1 text-left">
+                <div class="pb_icon"><i class="fab fa-accessible-icon fa-xs pb_icon-gradient"></i></div>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-3 heading">Personas con discapacidad</h5>
+                  <p class="text-sans-serif">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                    there live the blind texts.</p>
+                </div>
+              </div>
             </div>
-            <h3>Categories</h3>
+            <div class="col-lg-4 col-md- col-sm-6">
+              <div class="media d-block pb_feature-v1 text-left">
+                <div class="pb_icon"><i class="fas fa-globe-americas pb_icon-gradient"></i></div>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-3 heading">Migración y Salvadoreños en el mundo</h5>
+                  <p class="text-sans-serif">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                    there live the blind texts.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md- col-sm-6">
+              <div class="media d-block pb_feature-v1 text-left">
+                <div class="pb_icon"><i class="fab fa-envira pb_icon-gradient"></i></div>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-3 heading">Ambiente, agro y alimentos</h5>
+                  <p class="text-sans-serif">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                    there live the blind texts.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md- col-sm-6">
+              <div class="media d-block pb_feature-v1 text-left">
+                <div class="pb_icon"><i class="fas fa-theater-masks pb_icon-gradient"></i></div>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-3 heading">Cultura y Entretenimiento</h5>
+                  <p class="text-sans-serif">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                    there live the blind texts.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md- col-sm-6">
+              <div class="media d-block pb_feature-v1 text-left">
+                <div class="pb_icon"><i class="fas fa-lightbulb pb_icon-gradient"></i></div>
+                <div class="media-body">
+                  <h5 class="mt-0 mb-3 heading">Energía y Comunicaciones</h5>
+                  <p class="text-sans-serif">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                    there live the blind texts.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
--- src/screens/UI/Home.js
+++ src/screens/UI/Home.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import ProcedureSearch from '../../components/Procedure/Search';
+import Categories from '../UI/Categories'
 
 export default class Home extends Component {
   render() {
     return (
-      <section
-          className='pb_cover_v3 overflow-hidden cover-bg-indigo cover-bg-opacity text-left
-            pb_gradient_v1'
-          id='section-home'>
-        <div className='container'>
-          <div className='row align-items-center justify-content-center'>
-            <div className='col-md-6'>
-              <h1 className='mb-3 site-title'>gob<span className='title-dot'></span>sv </h1>
-              <div className='sub-heading'>
-                <p className='mb-4'>
-                  Bienvenido a tu portal para consulta de Trámites. Podrás encontrar información
-                  sobre todos los trámites del Gobierno de El Salvador.
-                </p>
+      <div>
+        <section
+            className='pb_cover_v3 overflow-hidden cover-bg-indigo cover-bg-opacity text-left
+              pb_gradient_v1'
+            id='section-home'>
+          <div className='container'>
+            <div className='row align-items-center justify-content-center'>
+              <div className='col-md-6'>
+                <h1 className='mb-3 site-title'>gob<span className='title-dot'></span>sv </h1>
+                <div className='sub-heading'>
+                  <p className='mb-4'>
+                    Bienvenido a tu portal para consulta de Trámites. Podrás encontrar información
+                    sobre todos los trámites del Gobierno de El Salvador.
+                  </p>
+                </div>
               </div>
+              <div className='col-md-1'>
+              </div>
+              <ProcedureSearch />
             </div>
-            <div className='col-md-1'>
-            </div>
-            <ProcedureSearch />
           </div>
-        </div>
-      </section>
+        </section>
+        <Categories />
+      </div>
     )
   }
 }
