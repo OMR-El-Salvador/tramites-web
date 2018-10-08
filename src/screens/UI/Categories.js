7010bf5545d945c050bad08e9ead0d76fa5dae89
Gual Escalante <gual23@gmail.com>
2018-10-08 16:51:31 -0600

    Updated categories load from remote

--- src/screens/UI/Categories.js
+++ src/screens/UI/Categories.js
@@ -1,154 +1,43 @@
 import React, { Component } from 'react';
+import '../../services/http';
+
 import './Categories.css';
+import { HttpService } from '../../services/http';
 
-const SERVER_URL = 'http://localhost:3000/';
-
-const categories = [
-  {
-    id: 1,
-    name: 'Subsidios y Programas Sociales',
-    iconClass: 'fas fa-hand-paper',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-                  'Consonantia, there live the blind texts'
-  },
-  {
-    id: 2,
-    name: 'Personas con discapacidad',
-    iconClass: 'fab fa-accessible-icon',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 3,
-    name: 'Vehículos y transporte',
-    iconClass: 'fas fa-shuttle-van',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 4,
-    name: 'Educación',
-    iconClass: 'fas fa-graduation-cap',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 5,
-    name: 'Construcción',
-    iconClass: 'fas fa-toolbox',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 6,
-    name: 'Inicio y operación de negocios',
-    iconClass: 'fas fa-industry',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 7,
-    name: 'Dinero y Obligaciones tributarias',
-    iconClass: 'fas fa-hand-holding-usd',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 8,
-    name: 'Pareja y Familia',
-    iconClass: 'fas fa-heart',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 9,
-    name: 'Migración',
-    iconClass: 'fas fa-globe-americas',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 10,
-    name: 'Trabajo y Obligaciones Patronales',
-    iconClass: 'fas fa-briefcase',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 11,
-    name: 'Ambiente, agro, alimentos y bebidas',
-    iconClass: 'fab fa-pagelines',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 12,
-    name: 'Cultura y Entretenimiento',
-    iconClass: 'fas fa-theater-masks',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 13,
-    name: 'Asociaciones y Fundaciones',
-    iconClass: 'fas fa-hands-helping',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 14,
-    name: 'Importaciones y Exportaciones',
-    iconClass: 'fas fa-atlas',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 15,
-    name: 'Energía y Comunicaciones',
-    iconClass: 'fas fa-lightbulb',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 16,
-    name: 'Seguridad, Defensa y Materiales peligrosos',
-    iconClass: 'fas fa-shield-alt',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 17,
-    name: 'Salud',
-    iconClass: 'fas fa-stethoscope',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  },
-  {
-    id: 18,
-    name: 'Identidad',
-    iconClass: 'fas fa-id-card',
-    description: 'Far far away, behind the word mountains, far from the countries Vokalia and ' +
-      'Consonantia, there live the blind texts'
-  }
-]
-
-const icoClass =
-  {
-    mag: 'fas fa-leaf',
-    marn: 'fas fa-tint',
-    mc: 'fas fa-theater-masks',
-    mdn: 'fas fa-shield-alt',
-    mgdt: 'fas fa-map',
-    mh: 'fas fa-clipboard-list',
-    minec: 'fas fa-money-bill-alt',
-    mined: 'fas fa-book',
-    minsal: 'fas fa-user-md',
-    mitur: 'fas fa-umbrella-beach',
-    mjsp: 'fas fa-balance-scale',
-    mop: 'fas fa-road',
-    mrree: 'fas fa-passport',
-    mtps: 'fas fa-industry',
-  }
+const icoClass = {
+  'SPS-001': 'fas fa-hand-paper',
+  'PD-001': 'fab fa-accessible-icon',
+  'VT-001': 'fas fa-shuttle-van',
+  'EDU-001': 'fas fa-graduation-cap',
+  'CON-001': 'fas fa-toolbox',
+  'ION-001': 'fas fa-industry',
+  'DOT-001': 'fas fa-hand-holding-usd',
+  'PF-001': 'fas fa-heart',
+  'MIG-001': 'fas fa-globe-americas',
+  'TOP-001': 'fas fa-briefcase',
+  'AAB-001': 'fab fa-pagelines',
+  'CE-001': 'fas fa-theater-masks',
+  'AF-001': 'fas fa-hands-helping',
+  'IE-001': 'fas fa-atlas',
+  'EC-001': 'fas fa-lightbulb',
+  'SDM-001': 'fas fa-shield-alt',
+  'SAL-001': 'fas fa-stethoscope',
+  'ID-001': 'fas fa-id-card',
+  MAG: 'fas fa-leaf',
+  MARN: 'fas fa-tint',
+  MC: 'fas fa-theater-masks',
+  MDN: 'fas fa-shield-alt',
+  MGDT: 'fas fa-map',
+  MH: 'fas fa-clipboard-list',
+  MINEC: 'fas fa-money-bill-alt',
+  MINED: 'fas fa-book',
+  MINSAL: 'fas fa-user-md',
+  MITUR: 'fas fa-umbrella-beach',
+  MJSP: 'fas fa-balance-scale',
+  MOP: 'fas fa-road',
+  MRREE: 'fas fa-passport',
+  MTPS: 'fas fa-industry',
+}
 
 const classes = [
   {
@@ -166,7 +55,7 @@ const classes = [
 export default class Categories extends Component {
   constructor (props) {
     super(props)
-    this.state = { groupBy: 'CAT', ministries: [] };
+    this.state = { groupBy: 'CAT', ministries: [], categories: [] };
 
     this.handleChange = this.handleChange.bind(this);
   }
@@ -174,12 +63,8 @@ export default class Categories extends Component {
   handleChange(evt) { this.setState({ groupBy: evt.target.value.toUpperCase() }); }
 
   componentDidMount() {
-    let resourceURL = 'institutions';
-    let reqURL = SERVER_URL + resourceURL;
-
-    fetch(reqURL)
-    .then(results => results.json())
-    .then(data => this.setState({ministries: data}));
+    HttpService.getResource('institutions').then(data => this.setState({ministries: data}));
+    HttpService.getResource('categories').then(data => this.setState({categories: data}));
   }
 
   render() {
@@ -206,11 +91,11 @@ export default class Categories extends Component {
             { this.state.groupBy === 'CAT' &&
               <div className='row'>
                 {
-                  categories.map(cat => (
+                  this.state.categories.map(cat => (
                     <div key={cat.id} className='col-lg-4 col-sm-6 d-flex align-items-stretch'>
                       <div className='pb_feature-v1 card text-center'>
                         <div className='pb_icon card-img-top'>
-                          <i className={cat.iconClass + ' pb_icon-gradient'}></i>
+                          <i className={icoClass[cat.code] + ' pb_icon-gradient'}></i>
                         </div>
                         <div className='media-body card-body d-flex flex-column'>
                           <h6 className='mt-0 mb-3 heading card-title'>{cat.name}</h6>
@@ -230,11 +115,7 @@ export default class Categories extends Component {
                     <div key={min.id} className='col-lg-3 col-sm-6 d-flex align-items-stretch mb-4'>
                       <div className='pb_feature-v1 card text-center'>
                         <div className='pb_icon card-img-top'>
-                          <i
-                            className={
-                              icoClass[min.code.toLowerCase()] +
-                              ' pb_icon-gradient-institutions'
-                            }>
+                          <i className={icoClass[min.code] + ' pb_icon-gradient-institutions'}>
                           </i>
                         </div>
                         <div className='media-body card-body d-flex flex-column'>
