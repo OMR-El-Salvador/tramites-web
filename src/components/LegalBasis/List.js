809f4e64e7895da3ba02c5ca6939ac259cd71b02
Gual Escalante <gual23@gmail.com>
2018-10-23 18:20:44 -0600

    Added modal display

--- src/components/LegalBasis/List.js
+++ src/components/LegalBasis/List.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 import PropTypes from 'prop-types';
 
 // const legislationTypes = {
@@ -13,28 +14,79 @@ import PropTypes from 'prop-types';
 //   'executive_order': 'Decreto ejecutivo'
 // }
 
+const modalStyles = {
+  content : {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement(document.getElementById('root'));
+
 export default class LegalBasisList extends Component {
+  constructor() {
+    super();
+
+    this.state = { modalIsOpen: false, selected: null };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal(i) { this.setState({modalIsOpen: true, selected: i}); }
+
+  closeModal() { this.setState({modalIsOpen: false}); }
+
   changeTopic(newTopic) { this.prevTopic = newTopic; };
 
   render() {
     let prevTopic = null;
 
     return (
-      <table className='table table-hover'>
-        <tbody>
-          {
-            this.props.legalBasis.map(lb => (
-              <tr key={lb.id}>
-                <td style={{fontWeight: '500'}}>
-                  {lb.legal_topic.name!==prevTopic && lb.legal_topic.name}
-                </td>
-                <td>{!lb.legislation_name?'No Existe':lb.legislation_name}</td>
-                {this.changeTopic(lb.legal_topic.name)}
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+      <div>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          style={modalStyles}
+          contentLabel='Detalles de base legal'
+        >
+          <button onClick={this.closeModal} type='button' className='close' aria-label='Close'>
+            <span aria-hidden='true'>&times;</span>
+          </button>
+          {this.state.selected &&
+          <div className='card border-light mb-3'>
+            <div className='card-header'>
+              {this.props.legalBasis[this.state.selected].legal_topic.name}
+            </div>
+            <div className='card-body text-dark'>
+              <h5 className='card-title'>
+                {this.props.legalBasis[this.state.selected].legislation_name}
+              </h5>
+              <p className='card-text'>
+                {this.props.legalBasis[this.state.selected].legislation_reference}
+              </p>
+            </div>
+          </div>}
+        </Modal>
+        <table className='table table-hover'>
+          <tbody>
+            {
+              this.props.legalBasis.map((lb, i) => (
+                <tr key={lb.id} onClick={() => this.openModal(i)}>
+                  <td style={{fontWeight: '500'}}>
+                    {lb.legal_topic.name!==prevTopic && lb.legal_topic.name}
+                  </td>
+                  <td>{!lb.legislation_name?'No Existe':lb.legislation_name}</td>
+                  {this.changeTopic(lb.legal_topic.name)}
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
