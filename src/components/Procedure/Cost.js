809fb7578d33565b3d89a99a7a849d553a10bc64
gual <gual23@gmail.com>
2018-10-26 15:00:52 -0600

    Updated costs component

--- src/components/Procedure/Cost.js
+++ src/components/Procedure/Cost.js
@@ -4,11 +4,12 @@ import PropTypes from 'prop-types';
 import { HttpService } from '../../services/http';
 import ProcedureCardElement from "./CardElement";
 import URL from '../../components/UI/URL/URL';
+import Loading from '../UI/Loading';
 
 const currencies = { 'dollar': '$', 'colon': '₡' }
 
 export default class ProcedureCost extends Component {
-  constructor(props) { super(props); this.state = { data: null, status: 'load' }; }
+  constructor(props) { super(props); this.state = { data: null, status: 'loading' }; }
 
   componentDidMount() {
     HttpService.getResource('costs', '?mode_id=eq.' + this.props.id).then(data => {
@@ -18,15 +19,41 @@ export default class ProcedureCost extends Component {
   }
 
   render() {
-    let detail;
-    if (this.props.link)
-      detail = <URL href={this.props.link} text='Ver archivo'/>;
-    else if (this.props.amount)
-      detail = currencies[this.props.currency] + this.props.amount.toFixed(2);
-    else
-      detail= 'No tiene'
-
-    return <ProcedureCardElement header={'Costo'} body={detail} />;
+    if (this.state.status === 'empty') return <p style={{fontWeight: '500'}}>No posee costos.</p>
+    else if (this.state.status === 'loading') return <Loading />
+    else {
+      return (
+        <div>
+          {this.state.data.amount &&
+          <ProcedureCardElement
+            header={'Monto'}
+            body={currencies[this.state.data.currency] + this.state.data.amount}
+          />}
+          {this.state.data.description &&
+          <ProcedureCardElement header={'Descripción'} body={this.state.data.description} />}
+          {this.state.data.main_url &&
+          <ProcedureCardElement
+            header={'Información'}
+            body={<URL href={this.state.data.main_url} text='Ver en línea' />}
+          />}
+          {this.state.data.secondary_url &&
+          <ProcedureCardElement
+            header={'Información complementaria'}
+            body={<URL href={this.state.data.main_url} text='Ver en línea' />}
+          />}
+          {this.state.data.main_file &&
+          <ProcedureCardElement
+            header={'Información'}
+            body={<URL href={this.state.data.main_file} text='Ver en línea' />}
+          />}
+          {this.state.data.secondary_file &&
+          <ProcedureCardElement
+            header={'Información complementaria'}
+            body={<URL href={this.state.data.main_file} text='Ver en línea' />}
+          />}
+        </div>
+      )
+    }
   }
 }
 
