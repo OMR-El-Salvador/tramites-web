42b7133058980b1f10c285e24b0b2ae33f7997d4
gual <gual23@gmail.com>
2018-10-28 11:46:31 -0600

    Removed template footer dependency, removed template files

--- src/screens/UI/Footer.js
+++ src/screens/UI/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <footer className='pb_footer bg-light' role='contentinfo'>
+      <footer className='footer'>
         <div className='container'>
           <div className='row text-center'>
             <div className='col'>
@@ -12,11 +12,6 @@ export default class Footer extends Component {
                 <li className='list-inline-item'><a href='https://twitter.com/OMR_sv' className='p-2'><i className='fab fa-twitter'></i></a></li>
                 <li className='list-inline-item'><a href='https://www.linkedin.com/company/omr-sv/' className='p-2'><i className='fab fa-linkedin'></i></a></li>
               </ul>
-            </div>
-          </div>
-          <div className='row'>
-            <div className='col text-center'>
-              <p className='pb_font-14'>Plantilla basada en 'Landing' por <a href='https://uicookies.com/'>uicookies.com</a></p>
             </div>
           </div>
         </div>
