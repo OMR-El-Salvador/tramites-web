0a2fd80d7d9044569c9b9b33d22b101724011b54
gual <gual23@gmail.com>
2018-10-28 12:02:47 -0600

    Updated footer info

--- src/screens/UI/Footer.js
+++ src/screens/UI/Footer.js
@@ -6,12 +6,16 @@ export default class Footer extends Component {
       <footer className='footer'>
         <div className='container'>
           <div className='row text-center'>
-            <div className='col'>
+            <div className='col-md-4 text-white'>Organismo de Mejora Regulatoria</div>
+            <div className='col-md-4'>
               <ul className='list-inline'>
                 <li className='list-inline-item'><a href='https://www.facebook.com/OMRsv/' className='p-2'><i className='fab fa-facebook'></i></a></li>
                 <li className='list-inline-item'><a href='https://twitter.com/OMR_sv' className='p-2'><i className='fab fa-twitter'></i></a></li>
                 <li className='list-inline-item'><a href='https://www.linkedin.com/company/omr-sv/' className='p-2'><i className='fab fa-linkedin'></i></a></li>
               </ul>
+            </div>
+            <div className='col-md-4'>
+              <a className='text-white' href='http://omr.gob.sv'>http://omr.gob.sv</a>
             </div>
           </div>
         </div>
