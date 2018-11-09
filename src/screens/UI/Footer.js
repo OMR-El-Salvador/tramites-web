80d86e266988473df7ad23a61d1e38c30fcd0408
gual <gual23@gmail.com>
2018-11-09 16:22:24 -0600

    Added logos to footer

--- src/screens/UI/Footer.js
+++ src/screens/UI/Footer.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
+import FooterImg from '../../img/footer.jpg'
+
+const itemStyle = {
+  fontSize: '2em',
+  marginLeft: '1em'
+}
 
 export default class Footer extends Component {
   render() {
     return (
       <footer className='footer'>
+        <hr />
         <div className='container'>
           <div className='row text-center'>
-            <div className='col-md-4 text-white'>Organismo de Mejora Regulatoria</div>
-            <div className='col-md-4'>
+            <div className='col-md-3 text-center'>
+              <a href='http://omr.gob.sv'>http://omr.gob.sv</a>
+            </div>
+            <div className='col-md-3'>
               <ul className='list-inline'>
-                <li className='list-inline-item'><a href='https://www.facebook.com/OMRsv/' className='p-2'><i className='fab fa-facebook'></i></a></li>
-                <li className='list-inline-item'><a href='https://twitter.com/OMR_sv' className='p-2'><i className='fab fa-twitter'></i></a></li>
-                <li className='list-inline-item'><a href='https://www.linkedin.com/company/omr-sv/' className='p-2'><i className='fab fa-linkedin'></i></a></li>
+                <li className='list-inline-item' style={itemStyle}>
+                  <a href='https://www.facebook.com/OMRsv/'><i className='fab fa-facebook'></i></a>
+                </li>
+                <li className='list-inline-item' style={itemStyle}>
+                  <a href='https://twitter.com/OMR_sv'><i className='fab fa-twitter'></i></a>
+                </li>
+                <li className='list-inline-item' style={itemStyle}>
+                  <a href='https://www.linkedin.com/company/omr-sv/'>
+                    <i className='fab fa-linkedin'></i>
+                  </a>
+                </li>
               </ul>
             </div>
-            <div className='col-md-4'>
-              <a className='text-white' href='http://omr.gob.sv'>http://omr.gob.sv</a>
+            <div className='col-md-6'>
+              <img src={FooterImg} alt='Participantes: MCC, FOMILENIO II, OMR, GOES' />
             </div>
           </div>
         </div>
