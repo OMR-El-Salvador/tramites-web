7f7c5e439a0a951f36de614917649e1da5cf922d
gual <gual23@gmail.com>
2018-11-09 15:37:53 -0600

    Added town filter for addresses

--- src/components/UI/Address/List.js
+++ src/components/UI/Address/List.js
@@ -5,7 +5,23 @@ import { HttpService } from '../../../services/http';
 import Address from './Address';
 
 export default class AddressesList extends Component {
-  constructor(props) { super(props); this.state = { id: this.props.id, data: {}, status: 'load' }; }
+  constructor(props) {
+    super(props);
+
+    this.state = { id: this.props.id, data: {}, status: 'load', term: '', filteredData: [] };
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+  }
+
+  handleFilterChange(ev) {
+    let term = ev.target.value;
+    let fData = [];
+    Object.keys(this.state.data).forEach((key) => (
+      this.state.data[key].addresses.forEach(addr => {
+        if (term && addr.municipality.toLowerCase().indexOf(term.toLowerCase()) > -1) fData.push(addr)
+      })
+    ))
+    this.setState({ term: term, filteredData: fData });
+  }
 
   componentDidMount() {
     let resPath = 'addresses';
@@ -47,45 +63,71 @@ export default class AddressesList extends Component {
 
   render() {
     return (
-      <div id='accordion' className='accordion'>
+      <div>
         {this.state.status === 'success' && (
-          Object.keys(this.state.data).map((key) => (
-            <div key={this.state.data[key].id} className='card border-light'>
-              <div
-                id={'heading-' + this.state.data[key].id}
-                className='card-header collapsed'
-                data-toggle='collapse'
-                data-target={'#collapse-' + this.state.data[key].id}
-                aria-expanded='false'
-                aria-controls={'collapse-' + this.state.data[key].id}
-                style={{backgroundColor: 'white'}}
-              >
-                <a style={{fontWeight: '450', fontSize: '1.2em'}}>
-                  {this.state.data[key].name}
-                </a>
+          <div>
+            <input
+              value={this.state.term}
+              onChange={this.handleFilterChange}
+              className='form-control'
+              placeholder='Filtrar por municipio'
+              type='text' />
+            {this.state.term === '' && (
+              <div id='accordion' className='accordion'>
+                {Object.keys(this.state.data).map((key) => (
+                  <div key={this.state.data[key].id} className='card border-light'>
+                    <div
+                      id={'heading-' + this.state.data[key].id}
+                      className='card-header collapsed'
+                      data-toggle='collapse'
+                      data-target={'#collapse-' + this.state.data[key].id}
+                      aria-expanded='false'
+                      aria-controls={'collapse-' + this.state.data[key].id}
+                      style={{backgroundColor: 'white'}}
+                    >
+                      <a style={{fontWeight: '450', fontSize: '1.2em'}}>
+                        {this.state.data[key].name}
+                      </a>
+                    </div>
+                    <div
+                      id={'collapse-' + this.state.data[key].id}
+                      className='collapse'
+                      aria-labelledby={'heading-'+this.state.data[key].id}
+                      data-parent='#accordion'>
+                      <div className='card-body text-secondary'>
+                        {this.state.data[key].addresses.map(addr => (
+                          <Address
+                            key={addr.id}
+                            detail={addr.detail}
+                            municipality={addr.municipality}
+                            schedule={addr.schedule}
+                            phone={addr.phone}
+                            responsible={addr.responsible}
+                            position={addr.position}
+                            email={addr.email}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
-              <div
-                id={'collapse-' + this.state.data[key].id}
-                className='collapse'
-                aria-labelledby={'heading-'+this.state.data[key].id}
-                data-parent='#accordion'>
-                <div className='card-body text-secondary'>
-                  {this.state.data[key].addresses.map(addr => (
-                    <Address
-                      key={addr.id}
-                      detail={addr.detail}
-                      municipality={addr.municipality}
-                      schedule={addr.schedule}
-                      phone={addr.phone}
-                      responsible={addr.responsible}
-                      position={addr.position}
-                      email={addr.email}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
-          ))
+            )}
+            {(this.state.term !== '' && this.state.filteredData.length > 0) && (
+              this.state.filteredData.map(addr => (
+                <Address
+                  key={addr.id}
+                  detail={addr.detail}
+                  municipality={addr.municipality}
+                  schedule={addr.schedule}
+                  phone={addr.phone}
+                  responsible={addr.responsible}
+                  position={addr.position}
+                  email={addr.email}
+                />
+              ))
+            )}
+          </div>
         )}
       </div>
     )
