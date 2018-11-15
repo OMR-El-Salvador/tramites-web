6d1d27cddf75c86fbd9ccf71fc7ae11c74a1d1a1
gual <gual23@gmail.com>
2018-11-15 10:14:02 -0600

    Added error handler to main app

--- src/App.js
+++ src/App.js
@@ -15,34 +15,40 @@ import ModeDetailScreen from './screens/Mode/Detail';
 import ModeListByCategoryScreen from './screens/Mode/ListByCategory';
 import Error from './components/UI/Error';
 import ScrollToTop from './components/UI/ScrollToTop';
+import ErrorHandler from './components/UI/ErrorHandler';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <ScrollToTop>
-          <div className='App'>
-            <NavBar />
-            <Route exact path='/' component={Home} />
-            {/* <Route path='/data' component={Data} /> */}
-            <Route path='/categorias' component={Categories} />
-            <Route path='/acercaDe' component={About} />
-            <Route path='/modalidades/:id([0-9]{1,6})' component={ModeDetailScreen} />
-            <Route path='/modalidades/categoria/:id([0-9]{1,2})' component={ModeListByCategoryScreen} />
-            <Route exact path='/tramites' component={ProcedureListScreen} />
-            <Route
-              path='/tramites/institucion/:id([0-9]{1,4})'
-              component={ProcedureListByInstitutionScreen}
-            />
-            <Route
-              path='/tramites/clase/:id([0-9]{1,2})'
-              component={ProcedureListByClassScreen}
-            />
-            <Route path='/404' render={() => <div className='text-center'><Error /></div>}/>
-            {/* <Redirect to='/404' /> */}
-            <Footer />
-          </div>
-        </ScrollToTop>
+        <ErrorHandler>
+          <ScrollToTop>
+            <div className='App'>
+              <NavBar />
+              <Route exact path='/' component={Home} />
+              {/* <Route path='/data' component={Data} /> */}
+              <Route path='/categorias' component={Categories} />
+              <Route path='/acercaDe' component={About} />
+              <Route path='/modalidades/:id([0-9]{1,6})' component={ModeDetailScreen} />
+              <Route
+                path='/modalidades/categoria/:id([0-9]{1,2})'
+                component={ModeListByCategoryScreen}
+              />
+              <Route exact path='/tramites' component={ProcedureListScreen} />
+              <Route
+                path='/tramites/institucion/:id([0-9]{1,4})'
+                component={ProcedureListByInstitutionScreen}
+              />
+              <Route
+                path='/tramites/clase/:id([0-9]{1,2})'
+                component={ProcedureListByClassScreen}
+              />
+              <Route path='/404' render={() => <div className='text-center'><Error /></div>}/>
+              {/* <Redirect to='/404' /> */}
+              <Footer />
+            </div>
+          </ScrollToTop>
+        </ErrorHandler>
       </Router>
     );
   }
